2d5321299d5d91aaddbcce3f47287748ee93c3d5
theripper93 <tsplab@gmail.com>
2021-08-17 16:10:38 +0200

    a

--- scripts/config.js
+++ scripts/config.js
@@ -30,6 +30,25 @@ Hooks.once("ready", async function () {
     type: String,
     default: "data.details.cr",
   });
+
+  game.settings.register("fuzzy-foundry", "excavateFilters", {
+    name: game.i18n.localize("fuzz.settings.excavateFilters.name"),
+    hint: game.i18n.localize("fuzz.settings.excavateFilters.hint"),
+    scope: "world",
+    config: true,
+    type: String,
+    default: "",
+  });
+
+  game.settings.register("fuzzy-foundry", "excavateWildcard", {
+    name: game.i18n.localize("fuzz.settings.excavateWildcard.name"),
+    hint: game.i18n.localize("fuzz.settings.excavateWildcard.hint"),
+    scope: "world",
+    config: true,
+    type: Boolean,
+    default: false,
+  });
+
   game.settings.register("fuzzy-foundry", "deepFile", {
     name: game.i18n.localize("fuzz.settings.deepFile.name"),
     hint: game.i18n.localize("fuzz.settings.deepFile.hint"),
@@ -37,15 +56,41 @@ Hooks.once("ready", async function () {
     config: true,
     type: Boolean,
     default: false,
-    onChange: (sett)=>{
-      if(sett) canvas.deepSearchCache = new FilePickerDeepSearch();
-    }
+    onChange: (sett) => {
+      if (sett) canvas.deepSearchCache = new FilePickerDeepSearch();
+    },
   });
-  if(game.settings.get("fuzzy-foundry", "deepFile"))canvas.deepSearchCache = new FilePickerDeepSearch();
+  if (game.settings.get("fuzzy-foundry", "deepFile"))
+    canvas.deepSearchCache = new FilePickerDeepSearch();
 });
 
-Hooks.on("getFilePickerHeaderButtons", () => {
-  
+Hooks.on("renderTokenConfig", (app, html) => {
+  if (!game.settings.get("fuzzy-foundry", "deepFile") || !game.user.isGM)
+    return;
+  let button = `<button type="button" id="excavator" class="file-picker" data-type="imagevideo" data-target="img" title="${game.i18n.localize(
+    "fuzz.tconfing.excavat.tip"
+  )}" tabindex="-1">
+  <i id="exicon" class="fab fa-digg"></i>
+</button>`;
+  html.find(".file-picker").after(button);
+  const name = app.object?.actor?.data?.name || app.object?.data?.name;
+  const exclude = game.settings.get("fuzzy-foundry", "excavateFilters").split(",").filter((s) => s !== "");
+  html.on("click", "#excavator", (e) => {
+    e.preventDefault();
+    const wildCheck = html.find(`input[name="randomImg"]`)[0] ? html.find(`input[name="randomImg"]`)[0].checked : false;
+    const isWildcard = wildCheck && game.settings.get("fuzzy-foundry", "excavateWildcard");
+    const btn = $(e.currentTarget);
+    btn.find("#exicon")[0].className = "fas fa-spinner fa-spin";
+    btn.prop("disabled", true);
+    setTimeout(() => {
+      const newPath = tokenExcavator.excavate(name, isWildcard, exclude);
+      if (newPath) html.find(".image")[0].value = newPath;
+      btn.prop("disabled", false);
+      btn.find("#exicon")[0].className = newPath
+        ? "fab fa-digg"
+        : "fas fa-times";
+    }, 150);
+  });
 });
 
 Object.byString = function (o, s) {
--- scripts/module.js
+++ scripts/module.js
@@ -3,7 +3,7 @@ class FilePickerDeepSearch {
     this._fileCache = [];
     this._fileNameCache = [];
     this._fileIndexCache = {};
-    if(game.user.isGM) this.buildCache();
+    if (game.user.isGM) this.buildCache();
   }
 
   async buildCache(dir = "./") {
@@ -19,30 +19,29 @@ class FilePickerDeepSearch {
     }
   }
 
-  static buildHtml(dmode,data){
-    switch(dmode){
+  static buildHtml(dmode, data) {
+    switch (dmode) {
       case "tiles":
-        return `<img width="100" height="100" title="${data.fn}" src="${data.fp}">`
+        return `<img width="100" height="100" title="${data.fn}" src="${data.fp}">`;
       case "thumbs":
         return `        <img width="48" height="48" src="${data.fp}">
-        <span class="filename">${data.fn}</span>`
+        <span class="filename">${data.fn}</span>`;
       case "list":
-        return `<i class="fas fa-file fa-fw"></i>${data.fn}`
+        return `<i class="fas fa-file fa-fw"></i>${data.fn}`;
       case "images":
         return `<img title="${data.fn}" src="${data.fp}">
-        <span class="filename">${data.fn}</span>`
+        <span class="filename">${data.fn}</span>`;
     }
   }
 
-  static _onSearchFilter(wrapped,event, query, rgx, html) {
-    console.log(this)
-    if(!game.settings.get("fuzzy-foundry", "deepFile") || !game.user.isGM){
+  static async _onSearchFilter(wrapped, event, query, rgx, html) {
+    if (!game.settings.get("fuzzy-foundry", "deepFile") || !game.user.isGM) {
       return wrapped(event, query, rgx, html);
     }
     if ((!query || query.length < 4) && !this.reset) {
       this.reset = true;
-      this.render(true)
-      $(html).find(`input[name="filter"]`).focus()
+      this.render(true);
+      $(html).find(`input[name="filter"]`).focus();
       return wrapped(event, query, rgx, html);
     }
     if (!query || query.length < 4) {
@@ -50,26 +49,38 @@ class FilePickerDeepSearch {
     } else {
       this.reset = false;
     }
+    await FilePickerDeepSearch.wait(800)
+    if($(html).find(`input[name="filter"]`).val() !== query) return
     const folder = $(html).find(`input[name="target"]`)[0].value;
-    const dmode = $(html).find(".display-mode.active")[0].dataset.mode
+    const dmode = $(html).find(".display-mode.active")[0].dataset.mode;
     const cache = canvas.deepSearchCache;
     const fs = FuzzySearchFilters.FuzzySet(cache._fileNameCache, true);
-    const queryRes= fs.get(query)
-    let qresult = queryRes ? queryRes.filter((e) => {
-      return e[0]>0.3}).map((r) => r[1]) || [] : [];
-    if(qresult.length == 0) {
-      return wrapped(event, query, rgx, html)
+    const queryRes = fs.get(query);
+    let qresult = queryRes
+      ? queryRes
+          .filter((e) => {
+            return e[0] > 0.3;
+          })
+          .map((r) => r[1]) || []
+      : [];
+    if (qresult.length == 0) {
+      return wrapped(event, query, rgx, html);
     }
     $("section.filepicker-body").html("");
     let $ol = $(`<ol class="directory files-list ${dmode}-list">`);
 
     for (let file of qresult) {
       const ext = "." + file.split(".").pop();
-      if((!cache._fileIndexCache[file]?.startsWith(folder))) continue
-      if(this.extensions && !this.extensions.includes(ext)) continue
-      let olHtml = `<li class="file${dmode == "thumbs" ? " flexrow" : ""}" data-path="${cache._fileIndexCache[file]}" draggable="true">`
-      olHtml+= FilePickerDeepSearch.buildHtml(dmode,{fn:file,fp:cache._fileIndexCache[file]})
-      olHtml += `</li>`
+      if (!cache._fileIndexCache[file]?.startsWith(folder)) continue;
+      if (this.extensions && !this.extensions.includes(ext)) continue;
+      let olHtml = `<li class="file${
+        dmode == "thumbs" ? " flexrow" : ""
+      }" data-path="${cache._fileIndexCache[file]}" draggable="true">`;
+      olHtml += FilePickerDeepSearch.buildHtml(dmode, {
+        fn: file,
+        fp: cache._fileIndexCache[file],
+      });
+      olHtml += `</li>`;
       $ol.append(olHtml);
     }
     $("section.filepicker-body").append($ol);
@@ -81,5 +92,66 @@ class FilePickerDeepSearch {
     this.activateListeners($(html));
     this.setPosition({ height: "auto" });
     $(html).find(`input[name="filter"]`).focus();
+  }
+
+  static wait(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+}
+
+class tokenExcavator {
+  static findCommon(s1, s2) {
+    //find the largest common substring between two strings
+    let maxl = 0;
+    let common = "";
+    for (let i = 0; i < s1.length; i++) {
+      for (let j = 0; j < s2.length; j++) {
+        let l = 0;
+        while (
+          i + l < s1.length &&
+          j + l < s2.length &&
+          s1[i + l] == s2[j + l]
+        ) {
+          l++;
+        }
+        if (l > maxl) {
+          maxl = l;
+          common = s1.substr(i, l);
+        }
+      }
+    }
+    return common;
+  }
+
+  static makeWildcard(s1, s2) {
+    //make a wildcard string that will match s1 and s2
+    let common = tokenExcavator.findCommon(s1, s2);
+    let wildcard = s1;
+    let parts = s1.split(common);
+    for (let part of parts) {
+      if (part !== "") wildcard = wildcard.replace(part, `*`);
+    }
+    return wildcard;
+  }
+
+  static excavate(query, isWildcard = false, exclude = []) {
+    exclude = exclude.map((e) => e.replace(/\s/g, "%20"));
+    const cache = canvas.deepSearchCache;
+    const fs = FuzzySearchFilters.FuzzySet(cache._fileNameCache, true);
+    const queryRes = fs.get(query, []).filter((q) => {
+      if (exclude.length == 0) return true;
+      for (let ex of exclude) {
+        if (cache._fileIndexCache[q[1]].includes(ex)) return true;
+      }
+      return false;
+    });
+    if (!queryRes || queryRes.length === 0) return undefined;
+    if (!isWildcard) return cache._fileIndexCache[queryRes[0][1]];
+    if (queryRes.length < 2) return undefined;
+    const path1 = cache._fileIndexCache[queryRes[0][1]];
+    const fileName1 = queryRes[0][1];
+    const fileName2 = queryRes[1][1];
+    const wildcard = tokenExcavator.makeWildcard(fileName1, fileName2);
+    return path1.replace(fileName1, wildcard);
   }
 }
